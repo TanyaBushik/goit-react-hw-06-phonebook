3a29faa3b095fce30bad80b87e0f8b08d34bbdf4
TanyaBushik <tanya.bushik@gmail.com>
2023-07-02 15:17:58 +0300

    deleted commented

--- src/components/ContactForm/ContactForm.styled.js
+++ src/components/ContactForm/ContactForm.styled.js
@@ -1,14 +1,6 @@
 import styled from '@emotion/styled';
 import { Form, Field, ErrorMessage } from 'formik';
 
-// export const Form = styled.form`
-//   padding: 10px;
-// display: flex;
-// flex-direction: column;
-// justify-content: center;
-// align-items: flex-start;
-// `;
-
 export const Container = styled(Form)`
   padding: 10px;
   display: flex;
@@ -65,57 +57,3 @@ export const Btn = styled.button`
     background-color: blue;
   }
 `;
-
-// export const NameInput = styled.input`
-// margin-top: 10px;
-// margin-bottom: 10px;
-// font-size: 15px;
-// font-weight: 400;
-// border-radius: 5px;
-// outline: none;
-// border: 1px solid black;
-// width: 200px;
-// height: 20px;
-// padding: 7px 15px;
-// `;
-
-// export const NumberInput = styled.input`
-//   margin-top: 10px;
-//   margin-bottom: 10px;
-//   font-size: 15px;
-//   font-weight: 400;
-//   border-radius: 5px;
-//   outline: none;
-//   border: 1px solid black;
-//   width: 200px;
-//   height: 20px;
-//   padding: 7px 15px;
-// `;
-
-// export const LabelName = styled.label`
-// font-size: 16px;
-// font-weight: 400;
-// display: flex;
-// flex-direction: column;
-// `;
-
-// export const LabelNumber = styled.label`
-//   font-size: 16px;
-//   font-weight: 400;
-//   display: flex;
-//   flex-direction: column;
-// `;
-
-// export const AddBtn = styled.button`
-// border-radius: 10px;
-// padding: 5px 10px;
-// font-size: 16px;
-// font-weight: 400;
-// border: 1px solid;
-// background-color: white;
-
-// &:hover {
-//   color: white;
-//   background-color: blue;
-// }
-// `;
